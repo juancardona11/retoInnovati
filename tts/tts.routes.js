e325a5dcc99a972ce2ecdbc149002d779d3e2051
Juan David Cardona <juan.cardona11@udea.edu.co>
2019-10-07 22:40:14 -0500

    update

--- tts/tts.routes.js
+++ tts/tts.routes.js
@@ -1,16 +1,18 @@
 const Tts = require('./tts.controller');
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  if (req.method === 'OPTIONS'){
-    res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type");
-    return res.status(200).json({});
-  }
-  next();
-});
+
 module.exports = (router) => {
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    if (req.method === 'OPTIONS'){
+      res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+      return res.status(200).json({});
+    }
+    next();
+  });
+  
   router.post('/textToSpeech',  Tts.conversation);
   router.get('/audio', Tts.audio);
 }
