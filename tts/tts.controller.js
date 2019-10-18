2df4b4a94d80ceef9ad368b18eadd3665556cf8b
Juan David Cardona <juan.cardona11@udea.edu.co>
2019-10-18 11:09:56 -0500

    Update faceApp

--- tts/tts.controller.js
+++ tts/tts.controller.js
@@ -1,19 +1,29 @@
+// Requires request and request-promise for HTTP requests
+// e.g. npm install request request-promise
 const request = require('request');
 const rp = require('request-promise');
+// Requires fs to write synthesized speech to a file
 const fs = require('fs');
+// Requires readline-sync to read command line inputs
 const readline = require('readline-sync');
+// Requires xmlbuilder to build the SSML body
 const xmlbuilder = require('xmlbuilder');
+
 const mediaserver = require('mediaserver');
+
 const path = require('path');
 
 exports.conversation = (request, response) => {
     
     async function main() {
         const subscriptionKey = "080373c5f65e4bb494a03794b9012e4c";
-        const text = request.body.text;
         if (!subscriptionKey) {
             throw new Error('Environment variable for your subscription key is not set.')
         };
+        // Prompts the user to input text.
+        //const text = readline.question('What would you like to convert to speech? ');
+        const text = request.body.text;
+        
         try {
         const accessToken = await getAccessToken(subscriptionKey);
         let request =  await textToSpeech(accessToken, text);
@@ -24,7 +34,7 @@ exports.conversation = (request, response) => {
     }
     main();
     
-    // Obtiene el token de acceso
+    // Gets an access token.
     function getAccessToken(subscriptionKey) {
         let options = {
             method: 'POST',
@@ -38,7 +48,7 @@ exports.conversation = (request, response) => {
 
     
     async  function textToSpeech(accessToken, text) {
-        // Se crea la petición SSML 
+        // Create the SSML request.
         let xml_body = xmlbuilder.create('speak')
             .att('version', '1.0')
             .att('xml:lang', 'es-mx')
@@ -47,6 +57,7 @@ exports.conversation = (request, response) => {
             .att('name', 'Microsoft Server Speech Text to Speech Voice (es-MX, HildaRUS)')
             .txt(text)
             .end();
+        // Convert the XML into a string to send in the TTS request.
         let body = xml_body.toString();
         let options = {
             method: 'POST',
@@ -62,8 +73,8 @@ exports.conversation = (request, response) => {
             body: body
         }
          try{
-           //Se utiliza la función getAudio para optener el audio del texto
-          var res = await getAudio(options);      
+           let result = await getAudio(options);  
+           response.send({message:'hola'});   
          } catch {}
     }          
 }
@@ -78,7 +89,7 @@ exports.conversation = (request, response) => {
             });  
          return request;
      }
-     //Se utiliza la librería mediaServer para enviar el archivo de audio al Front
+
      exports.audio = (request, response) => {
         var file = path.join(__dirname, '../','TTSOutput.wav');
         mediaserver.pipe(request, response, file);
