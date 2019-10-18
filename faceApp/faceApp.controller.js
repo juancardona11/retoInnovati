52faff90da0bdb1be658c089dfbed66e1d4534c4
Juan David Cardona <juan.cardona11@udea.edu.co>
2019-10-18 13:39:06 -0500

    Update face/audio

--- faceApp/faceApp.controller.js
+++ faceApp/faceApp.controller.js
@@ -5,22 +5,22 @@ exports.face = (request, response) => {
 
     async function main() {
         const subscriptionKey = 'f0d7619cbda245a9a12b49eabb3e3b88';
+
         if (!subscriptionKey) {
             throw new Error('Environment variable for your subscription key is not set.')
         };
+        //Se captura la imagen en base64 enviada desde el front
         const imageUrl = request.body.img;
+        //Se transforma la imagen y se guarda en un buffer
         var buff = new Buffer(imageUrl.replace(/^data:image\/(png|gif|jpeg);base64,/, ''), 'base64');
-        
         const uriBase = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect';
-        // Request parameters.
-        
+        //Se configuran los parametros que serán retornados por la API de Azure
         const params = {
             'returnFaceId': 'true',
             'returnFaceLandmarks': 'false',
             'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
                 'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
         };
-
         const options = {
             uri: uriBase,
             qs: params,
@@ -31,7 +31,7 @@ exports.face = (request, response) => {
             }
         };
         try {
-       //let result = JSON.parse(await faceApp(options));
+        //Utilizamos la función faceApp para consumir la API de Azure
         let result = JSON.stringify(await faceApp(options));
         response.send(result);
         } catch (err) {
@@ -41,8 +41,8 @@ exports.face = (request, response) => {
     }
     main();
 }
-
-        function faceApp(options){
+    //Se consume la API Face de Azure
+    function faceApp(options){
             return new Promise ( resolve => {
                 request.post(options, (error, response, body) => {
                     if (error) {
@@ -50,11 +50,10 @@ exports.face = (request, response) => {
                     return;
                     }
                     let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
-                    resolve(jsonResponse);
-                    
+                    resolve(jsonResponse);  
                 });
             });
-        }
+    }
       
 
 
